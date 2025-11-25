0fff18c93481eaff2b4fe6eb97f9deb9ba3cff03
Stefan Josipovic <stefan.josipovic2005@gmail.com>
2025-11-25 20:39:51 +0100

    feat: add Student entity

---
+++ src/main/java/org/example/canteenreservationsystem/entity/Student.java
@@ -0,0 +1,32 @@
+package org.example.canteenreservationsystem.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    private boolean admin;
+}
