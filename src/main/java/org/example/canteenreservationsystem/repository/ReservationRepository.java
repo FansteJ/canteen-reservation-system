9c5f00bcb5824e2bc7c4d0aaa9ce4b513daa75e1
Stefan Josipovic <stefan.josipovic2005@gmail.com>
2025-11-25 21:57:20 +0100

    feat: update repositories to contain basic methods

--- src/main/java/org/example/canteenreservationsystem/repository/ReservationRepository.java
+++ src/main/java/org/example/canteenreservationsystem/repository/ReservationRepository.java
@@ -3,5 +3,11 @@ package org.example.canteenreservationsystem.repository;
 import org.example.canteenreservationsystem.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findByStudentId(Long studentId);
+
+    List<Reservation> findByCanteenIdAndReservationDate(Long canteenId, Date reservationDate);
 }
--- src/main/java/org/example/canteenreservationsystem/repository/StudentRepository.java
+++ src/main/java/org/example/canteenreservationsystem/repository/StudentRepository.java
@@ -3,5 +3,8 @@ package org.example.canteenreservationsystem.repository;
 import org.example.canteenreservationsystem.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findByEmail(String email);
 }
